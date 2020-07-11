11e422c1f85adbf79d0749aa3ea39a5ab109b1fa
mohitraj-632 <60576681+mohitraj-632@users.noreply.github.com>
2020-07-11 19:51:45 +0530

    Update loco_main.py

--- loco_main.py
+++ loco_main.py
@@ -14,7 +14,7 @@ from websocket import create_connection
 from bs4 import BeautifulSoup
 
 bearer = "eyJhbGciOiJIUzl1NilslnR5cCl6lkpXVCJ9.eyJ1c2VybmFtZSI6lk1vaGloiFJhailsinVzZxJfdWIkljoiNDEWMjQOMTk30DY4MzgxNSIslmF2YXRhcil6lilslmV4cCI6MTU5NDQ3NTgxMSwiaWFOljoxNTkONDcOMDExLCJfX2FwcCI6InZIZGFudHUifQ.Vn02pu5DyWbRIS32mElW_brOEwsNMTqKejqWP_OAUXY"
-webhook_url = ""
+webhook_url = "https://discordapp.com/api/webhooks/731515307939856385/EWR3jDN-rcTWZipIN-VwxmcM3WM8Fc6PWsHSOHk9y-JQy1AkCBLOBcpMd5hiB9u7Nkge"
 
 hook = Webhook(webhook_url)
 try:
@@ -33,7 +33,7 @@ headers = {
 	'Ipn:':'com.vedantu.app',
 	'client-id':'CagkWJoLLYEY2FK9Ve2XqvekDkDDIITJnYkhpmfT',
 	'client-secret':'jbwPDmTe6eHSFnf7qSMOrQdOqZ1X05yoLPCw5jyGeXmRmmSi1hwRIYX7uFTuXdSmWcjfjrujFTnZM6UhXBUE7SGEg3zHgtH2nLk3Pxg6tAhD1Cyzlt590Rtl8T5GFJ0s',
-	'authorization':'eyJhbGciOiJIUzl1NilslnR5cCl6lkpXVCJ9.eyJ1c2VybmFtZSI6lk1vaGloiFJhailsinVzZxJfdWIkljoiNDEWMjQOMTk30DY4MzgxNSIslmF2YXRhcil6lilslmV4cCI6MTU5NDQ3NTgxMSwiaWFOljoxNTkONDcOMDExLCJfX2FwcCI6InZIZGFudHUifQ.Vn02pu5DyWbRIS32mElW_brOEwsNMTqKejqWP_OAUXY',
+	'authorization':'Bearer eyJhbGciOiJIUzl1NilslnR5cCl6lkpXVCJ9.eyJ1c2VybmFtZSI6lk1vaGloiFJhailsinVzZxJfdWIkljoiNDEWMjQOMTk30DY4MzgxNSIslmF2YXRhcil6lilslmV4cCI6MTU5NDQ3NTgxMSwiaWFOljoxNTkONDcOMDExLCJfX2FwcCI6InZIZGFudHUifQ.Vn02pu5DyWbRIS32mElW_brOEwsNMTqKejqWP_OAUXY',
 	'accept-encoding':'gzip',
 	'user-agent':'okhttp/3.14.4'
 }
